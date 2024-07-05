d602ca2bc1f451af2a682c08ffe578c018bb1ca8
keralex <kerlis.aguado26@gmail.com>
2024-07-04 23:44:34 -0300

    add navigation

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,14 +1,10 @@
 import ChatComponent from "@/components/ChatComponent";
 import Layout from "@/components/layout";
 
-
-export default function Home() {
-  return (
-    <Layout>
-      <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
+export default function ChatPage() {
+    return <Layout> <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
         <h2 className="text-2xl"> chatbot UWU</h2>
         <ChatComponent />
-      </div>
-    </Layout>
-  );
+    </div></Layout>
+
 }
--- components/Hearder.tsx
+++ components/Hearder.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 const Header = () => {
     return <nav>
         <ul className="flex mb-12">
             <li className="mr-6">
-                <a className="text-blue-500 hover:text-blue-800" href="#">Characters</a>
+                <Link className="text-blue-500 hover:text-blue-800" href="/characters">Characters</Link>
             </li>
             <li className="mr-6">
-                <a className="text-blue-500 hover:text-blue-800" href="#">Chat </a>
+                <Link className="text-blue-500 hover:text-blue-800" href="/chat">Chat </Link>
             </li>
         </ul>
     </nav>
